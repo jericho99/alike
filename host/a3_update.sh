95f033162518cfd002a6f00156ea6f94be31cb8d
ddoyon <ddoyon@sikv.net>
2025-05-08 09:47:35 -0400

    change url path

--- host/a3_update.sh
+++ host/a3_update.sh
@@ -62,11 +62,11 @@ echo "0 2 * * *    /usr/local/sbin/docker-clean.sh 2&>1 > /dev/null" | crontab -
 
 if [ ! -f "/home/alike/docker-compose.yml" ]; then
 	echo "Downloading Alike default contigs"
-	wget -qO /home/alike/docker-compose.yml https://raw.githubusercontent.com/quadricsoftware/alike/main/docker/docker-compose.yml 
+	wget -qO /home/alike/docker-compose.yml https://raw.githubusercontent.com/jericho99/alike/main/docker/docker-compose.yml 
 else
 	echo "Existing docker-compose.yml found.  Skipping update, using existing config."
 fi
-wget -qO - 'https://raw.githubusercontent.com/quadricsoftware/alike/main/host/a3.rev.num' > /usr/local/sbin/rev.num
+wget -qO - 'https://raw.githubusercontent.com/jericho99/alike/main/host/a3.rev.num' > /usr/local/sbin/rev.num
 echo 
 if [ ${sysUpdate} -eq 1 ]; then
 	echo "Updating A3 system packages"
